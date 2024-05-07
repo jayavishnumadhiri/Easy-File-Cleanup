f39f95cdb6452174183795757adca3c072303280
Madhiri Jayavishnu <104913389+jayavishnumadhiri@users.noreply.github.com>
2024-05-07 19:39:07 +0530

    Add files via upload

---
+++ Deleting Useless files in  folder.ps1
@@ -0,0 +1,46 @@
+﻿cls
+$c=0
+$f = Get-ChildItem -Path E:\Downloads | Where-Object{$_.LastAccessTime -le (Get-Date).AddDays(-366)}
+$f
+
+$data = @()
+
+$f | ForEach-Object {
+      $co = [PSCustomObject]@{
+        
+    "index" = $c++
+    "Filename" = $_.Name
+    "fileSize_mb" = [math]::Round(($_.Length/1mb),2)
+    "Path" = $_.FullName
+    
+      
+      }
+
+      $data += $co
+
+}
+
+$data | Format-Table
+
+
+
+
+$inputString = Read-Host "Enter indices of the files u dont want to delete seperate by Comma ','"
+
+$filesnotremove = $inputString.Split(',') | ForEach-Object { [int]$_ } | Sort-Object 
+
+$mem = 0
+
+$data | ForEach-Object {
+
+   if ($_.index -notin $filesnotremove){
+        
+       $mem +=$_.fileSize_mb
+        Remove-Item -Path $_.path -Force -WhatIf
+   
+   } 
+  
+}
+
+Write-Host "afer deletion free up memory "$mem -BackgroundColor Red
+
