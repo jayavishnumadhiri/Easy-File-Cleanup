7a32f5f28def14e986f9d0c999e28d4a87aec433
Madhiri Jayavishnu <104913389+jayavishnumadhiri@users.noreply.github.com>
2024-05-07 20:12:22 +0530

    Update Deleting Useless files in  folder.ps1

--- Deleting Useless files in  folder.ps1
+++ Deleting Useless files in  folder.ps1
@@ -1,5 +1,6 @@
 ﻿cls
 $c=0
+# Give Folder path here
 $f = Get-ChildItem -Path E:\Downloads | Where-Object{$_.LastAccessTime -le (Get-Date).AddDays(-366)}
 $f
 
@@ -36,7 +37,8 @@ $data | ForEach-Object {
    if ($_.index -notin $filesnotremove){
         
        $mem +=$_.fileSize_mb
-        Remove-Item -Path $_.path -Force -WhatIf
+       
+        Remove-Item -Path $_.path -Force -WhatIf  # Remove -whatIf To delete the files 
    
    } 
   
